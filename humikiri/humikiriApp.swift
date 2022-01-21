e3e6cf1442fc488b186ace8612e8676679f87e81
huki-no <saki-_-0117@ezweb.ne.jp>
2022-01-21 11:15:09 +0900

    Initial Commit

---
+++ humikiri/humikiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  humikiriApp.swift
+//  humikiri
+//
+//  Created by 安達咲希 on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct humikiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
